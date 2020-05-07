80fbaf9e97aaa038c9ef4ede6977c57e4f1f4b73
KevY <kevin.hendrews@gmail.com>
2020-05-07 15:51:10 +0500

    Remove server

--- server/src/ScrInternal.h
+++
@@ -1,5 +0,0 @@
-/*
-	Updated to 0.3.7 by P3ti
-*/
-
-void generateAndLoadInternalScript(lua_State *L);
--- server/src/netgame.h
+++
@@ -1,7 +0,0 @@
-/*
-	Updated to 0.3.7 by P3ti
-*/
-
-void resetPools();
-void UpdateNetwork();
-void UpdatePosition(int iPlayerID, float fX, float fY, float fZ);
